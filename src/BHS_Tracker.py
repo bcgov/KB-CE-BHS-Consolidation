a3bf7db067cf12703dcc39ac59d163304dcf2f7b
ElocF <cole.folkers@gov.bc.ca>
2024-01-23 16:22:17 -0800

    more syntax and upgrades

--- src/BHS_Tracker.py
+++ src/BHS_Tracker.py
@@ -5,23 +5,23 @@ from datetime import date
 class tracker: 
 
     def initialize(tracker_loc):
-    if not os.path.exists(tracker_loc):
-        wb= pyxl.Workbook()
-        ws=wb.active
-        ws1= wb.create_sheet('Data_Tracking',0)
-        header=['Date updated', 'feature class', 'Number of new features', 'Source', 'Updated by']
-        header_style=pyxl.styles.Font(size=12, bold=True)
-        ws1=wb['Data_Tracking']
-        ws1.append(header)
-        for cell in ws1["1:1"]:
-            cell.font=header_style
-        wb.save(tracker_loc)
-    else:
-        wb= pyxl.load_workbook(tracker_loc)
-        ws1=wb['Data_Tracking']
+        if not os.path.exists(tracker_loc):
+            wb= pyxl.Workbook()
+            ws=wb.active
+            ws1= wb.create_sheet('Data_Tracking',0)
+            header=['Date updated', 'feature class', 'Number of new features', 'Source', 'Updated by']
+            header_style=pyxl.styles.Font(size=12, bold=True)
+            ws1=wb['Data_Tracking']
+            ws1.append(header)
+            for cell in ws1["1:1"]:
+                cell.font=header_style
+            wb.save(tracker_loc)
+        else:
+            wb= pyxl.load_workbook(tracker_loc)
+            ws1=wb['Data_Tracking']
 
     def append_tracker(fc_name,feats,method,username):
-    xlsx_append=[date.today, fc_name, feats, method, username]
-            print(xlsx_append)
-            ws1.append(xlsx_append)
-    
+        xlsx_append=[date.today, fc_name, feats, method, username]
+        print(xlsx_append)
+        ws1.append(xlsx_append)
+        
